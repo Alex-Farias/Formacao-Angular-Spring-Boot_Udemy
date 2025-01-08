a28fa3fca84aa9483d184ebb84cfeff8ff0b6371
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2025-01-08 09:48:08 -0300

    initial commit

---
+++ src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/FormacaoAngularSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.zoltar.Formacao_Angular_Spring_Boot_Udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormacaoAngularSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormacaoAngularSpringBootUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/FormacaoAngularSpringBootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoltar.Formacao_Angular_Spring_Boot_Udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormacaoAngularSpringBootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
