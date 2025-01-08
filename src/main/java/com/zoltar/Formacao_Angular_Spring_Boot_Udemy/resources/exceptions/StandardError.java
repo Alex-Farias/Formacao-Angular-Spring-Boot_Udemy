181a78241b12bf972ec4db375272c486d1ba3001
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2025-01-08 17:06:10 -0300

    Aula 12 - Exceção para objeto não encontrado (Curso DEV Full Stack)

---
+++ src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/resources/exceptions/StandardError.java
@@ -0,0 +1,59 @@
+package com.zoltar.Formacao_Angular_Spring_Boot_Udemy.resources.exceptions;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private long timestamp;
+	private Integer status;
+	private String error;
+	private String message;
+	private String path;
+	public StandardError() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public StandardError(long timestamp, Integer status, String error, String message, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public String getError() {
+		return error;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+}
--- src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/services/TecnicoService.java
+++ src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.Tecnico;
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.repositories.TecnicoRepository;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -15,6 +16,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID:" + id));
 	}
 }
