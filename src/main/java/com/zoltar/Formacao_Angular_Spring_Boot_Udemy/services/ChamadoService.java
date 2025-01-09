70af057686165bb84b524a97193fa585c3bf7882
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2025-01-09 17:13:21 -0300

    Aula 22 - create (Chamado) (Curso DEV Full Stack)

--- src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/services/ChamadoService.java
+++ src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/services/ChamadoService.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.Chamado;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.Cliente;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.Tecnico;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.dtos.ChamadoDTO;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.enums.Prioridade;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.enums.Status;
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.repositories.ChamadoRepository;
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.services.exceptions.ObjectnotFoundException;
 
@@ -14,6 +19,10 @@ import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.services.exceptions.Objectn
 public class ChamadoService {
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -22,5 +31,25 @@ public class ChamadoService {
 
 	public List<Chamado> findAll() {
 		return repository.findAll();
+	}
+
+	public Chamado create(ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
 	}
 }
