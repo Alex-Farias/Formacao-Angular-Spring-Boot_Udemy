ffd2e75a30deb6d041587ae31bd1b3657c37c0b8
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2025-01-09 14:15:13 -0300

    Aula 15 - Exceção para CPF já cadastrado (Curso DEV Full Stack)

--- src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/repositories/PessoaRepository.java
+++ src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/repositories/PessoaRepository.java
@@ -1,9 +1,12 @@
 package com.zoltar.Formacao_Angular_Spring_Boot_Udemy.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
-
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
--- src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/services/TecnicoService.java
+++ src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/services/TecnicoService.java
@@ -6,15 +6,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.Pessoa;
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.Tecnico;
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.dtos.TecnicoDTO;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.repositories.PessoaRepository;
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.repositories.TecnicoRepository;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.services.exceptions.DataIntegrityViolationException;
 import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
 	@Autowired
 	private TecnicoRepository repository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
@@ -27,7 +32,20 @@ public class TecnicoService {
 
 	public Tecnico create(TecnicoDTO objDTO) {
 		objDTO.setId(null);
+		validaPorCpgEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
+	}
+
+	private void validaPorCpgEEmail(TecnicoDTO objDTO) {
+		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+		}
+		
+		obj = pessoaRepository.findByEmail(objDTO.getEmail());
+		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+		}
 	}
 }
