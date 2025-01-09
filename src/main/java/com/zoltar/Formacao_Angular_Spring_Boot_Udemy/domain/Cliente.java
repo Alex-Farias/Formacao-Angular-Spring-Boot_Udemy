1d5194b98713be3d0d4ffe0d0a6fa9daf1eb4c24
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2025-01-09 16:40:34 -0300

    Aula 19 - CRUD para Cliente (Curso DEV Full Stack)

--- src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/domain/Cliente.java
+++ src/main/java/com/zoltar/Formacao_Angular_Spring_Boot_Udemy/domain/Cliente.java
@@ -2,8 +2,10 @@ package com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.zoltar.Formacao_Angular_Spring_Boot_Udemy.domain.dtos.ClienteDTO;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
@@ -27,6 +29,17 @@ public class Cliente extends Pessoa{
 		super(id, nome, cpf, email, senha);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+ 	}
 
 	public List<Chamado> getChamados() {
 		return chamados;
