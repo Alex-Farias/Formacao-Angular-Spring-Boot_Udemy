4152f5a8331d71c25775b5a73c6fd470bc24a2a9
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2025-01-28 10:29:44 -0300

    Aula 36 - Componente LOGIN (Parte 2) (Curso DEV Full Stack)

--- Frontend/helpdesk/src/app/components/login/login.component.ts
+++ Frontend/helpdesk/src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Credenciais } from 'src/app/models/credenciais';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  creds: Credenciais = {
+    email: '',
+    senha: '',
+  }
+  
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
 
-  constructor() { }
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
   }
 
+  logar() {
+    this.toast.error('Usuário e/ou senha inválidos!', 'Login')
+    this.creds.senha=""
+  }
+
+  validaCampos(): boolean {
+    let b;
+    b = false
+    if(this.email.valid && this.senha.valid) {
+      b = true;
+    }
+    return b
+  }
 }
