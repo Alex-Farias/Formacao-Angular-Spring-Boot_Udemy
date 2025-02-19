2338129b765b6b10c31a902744cf6f212f6c4f6a
Alex Farias de Abreu Nabo <73539132+Alex-Farias@users.noreply.github.com>
2025-02-19 16:58:22 -0300

    versão node especificado para vercel

--- Frontend/helpdesk/src/app/config/api.config.ts
+++ Frontend/helpdesk/src/app/config/api.config.ts
@@ -1,3 +1,4 @@
 export const API_CONFIG = {
-    baseUrl : 'http://localhost:8080'
+    // baseUrl : 'http://localhost:8080'
+    baseUrl : 'https://helpdesk-prod-alex-7419428755fb.herokuapp.com/'
 }
